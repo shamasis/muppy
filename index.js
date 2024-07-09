f25d1f68db935ce5ebfd8bc4695eee2cb3aa9c33
Shamasis Bhattacharya <shamasis@postman.com>
2024-07-09 16:13:42 +0530

    Added ability to CLI provide OpenAI key in case it is not saved

--- index.js
+++ index.js
@@ -13,17 +13,44 @@
  * To exit the conversation, press ESC or Ctrl+C.
  */
 
-const OpenAI = require('openai');
-const prompts = require('prompts');
+const OpenAI = require('openai'),
+  prompts = require('prompts'),
+  colors = require('colors/safe');
 
-// Initialize OpenAI with API key
-const openai = new OpenAI({
-  // For extra safety, use the line below and pass the API key as an environment variable
-  apiKey: process.env['OPENAI_API_KEY']
-});
+
+// this function is used to setup OpenAI JS SDK.
+async function setup (apiKey) {
+  // check presence of Open AI API Key in environement. if not, gather from CLI
+  if (!apiKey) {
+    console.log (
+      'You need a valid OpenAI API Key to use muppy. Get yours from\n' +
+      colors.underline('https://platform.openai.com/api-keys') + '\n\n' +
+
+      colors.dim('Muppy does not save or log your API Key. To help Muppy find and use ' +
+      'your saved API Keys, save the API key in your environment variable as ' +
+      colors.bold('OPENAI_API_KEY') + '. You can set environment varable ' +
+      'using the command export OPENAI_API_KEY="your-api-key" ' + 
+      '(or setx OPENAI_API_KEY "your-api-key" on Windows.)\n')
+    );
+
+    const response = await prompts({
+      type: 'password',
+      name: 'apiKey',
+      message: 'Enter OpenAI API Key',
+      description: 'aaa'
+    }, { onCancel: () => process.exit(0) });
+
+    apiKey = response.apiKey;
+    console.log('\n');
+  }
+
+  return new OpenAI({ // Initialize OpenAI with API key
+    apiKey: apiKey
+  });
+}
 
 // Function to handle user input and generate AI response
-async function chatloop (messages) {
+async function chatloop (messages, openai) {
   const response = await prompts({
     type: 'text',
     name: 'input',
@@ -53,15 +80,18 @@ async function chatloop (messages) {
       messages.push({ role: 'assistant', content: aiResponse });
       console.log('\n');
       // Recurse to continue the conversation
-      chatloop(messages);
+      chatloop(messages, openai);
     });
 }
 
 // Start the conversation loop
-function main () {
-  const messages = [];
-  console.log("Start chatting with the AI. Press ESC or Ctrl+C to stop.");
-  chatloop(messages);
+async function main () {
+  // For extra safety, pass the API key as an environment variable
+  const openai = await setup(process.env['OPENAI_API_KEY']),
+    messages = [];
+
+  console.log(`Start chatting with the AI. ${colors.dim("Press ESC or Ctrl+C to stop.")}`);
+  chatloop(messages, openai);
 }
 
 // Invoke main function
